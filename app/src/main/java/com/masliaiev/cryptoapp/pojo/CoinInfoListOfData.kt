bcbcd7137e7d165c7f2729f0360c4264a3d2a3e3
MasliaievKirill <kirillmaslyaev@gmail.com>
2022-01-05 20:10:10 +0200

    Initial commit

---
+++ app/src/main/java/com/masliaiev/cryptoapp/pojo/CoinInfoListOfData.kt
@@ -0,0 +1,14 @@
+package com.masliaiev.cryptoapp.pojo
+
+import com.google.gson.annotations.Expose
+
+import com.google.gson.annotations.SerializedName
+
+
+
+
+data class CoinInfoListOfData  (
+    @SerializedName("Data")
+    @Expose
+    val data: List<Datum>? = null
+)

f3a848136d35f55d30579483c7c93abf005ad8ca
MasliaievKirill <kirillmaslyaev@gmail.com>
2022-01-20 13:11:29 +0200

    Use CoinDetailFragment.kt in CoinDetailActivity.kt

--- app/src/main/java/com/masliaiev/cryptoapp/presentation/CoinDetailActivity.kt
+++ app/src/main/java/com/masliaiev/cryptoapp/presentation/CoinDetailActivity.kt
@@ -5,12 +5,12 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import com.masliaiev.cryptoapp.R
 import com.masliaiev.cryptoapp.databinding.ActivityCoinDetailBinding
 import com.squareup.picasso.Picasso
 
 class CoinDetailActivity : AppCompatActivity() {
 
-    private lateinit var viewModel: CoinViewModel
     private val binding by lazy {
         ActivityCoinDetailBinding.inflate(layoutInflater)
     }
@@ -23,21 +23,12 @@ class CoinDetailActivity : AppCompatActivity() {
             return
         }
         val fromSymbol = intent.getStringExtra(EXTRA_FROM_SYMBOL) ?: EMPTY_SYMBOL
-        viewModel = ViewModelProvider(this)[CoinViewModel(application)::class.java]
-        viewModel.getDetailInfo(fromSymbol).observe(this) {
-            with(binding) {
-                tvPrice.text = it.price.toString()
-                tvMinPrice.text = it.lowDay.toString()
-                tvMaxPrice.text = it.highDay.toString()
-                tvLastMarket.text = it.lastMarket.toString()
-                tvLastUpdate.text = it.lastUpdate
-                tvFromSymbol.text = it.fromSymbol
-                tvToSymbol.text = it.toSymbol
-                Picasso.get().load(it.imageUrl).into(ivLogoCoinDetail)
-            }
-
-
+        if (savedInstanceState == null){
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, CoinDetailFragment.newInstance(fromSymbol))
+                .commit()
         }
+
 
     }
 
